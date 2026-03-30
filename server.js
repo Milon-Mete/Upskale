845c176a5885a073db75df674136764ffe276676
Milon Mete <milonmete07@gmail.com>
2026-03-30 22:59:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const PORT = process.env.PORT || 5000;
 // 🔴 SECURE CORS FOR COOKIES
 app.use(cors({
     origin: [
-        'http://localhost:5173', 
         'https://upskale-demo.netlify.app'
     ],
     credentials: true 
